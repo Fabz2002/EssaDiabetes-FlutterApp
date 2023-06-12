92af738a9eef790be702745fa9769875b4ab1362
Narizor <134035832+Narizor@users.noreply.github.com>
2023-06-12 12:34:35 -0500

    Update preSettings.view.dart

--- lib/view/preSettings.view.dart
+++ lib/view/preSettings.view.dart
@@ -1,120 +1,163 @@
 import 'package:first_app_flutter/Routes/routes.dart';
-import 'package:first_app_flutter/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PreSettingsView extends StatefulWidget {
-  const PreSettingsView({super.key});
+  final String id;
+  const PreSettingsView({Key? key, required this.id}) : super(key: key);
 
   @override
   State<PreSettingsView> createState() => _PreSettingsViewState();
 }
 
 class _PreSettingsViewState extends State<PreSettingsView> {
+  late String id;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    id = widget.id;
+  }
+
   int _selectedButtonIndex = 0;
+  double selectednivelActividad = 1.2;
 
   void _selectButton(int index) {
+    double nivelActividad;
+    switch (index) {
+      case 0:
+        nivelActividad = 1.2;
+        break;
+      case 1:
+        nivelActividad = 1.375;
+        break;
+      case 2:
+        nivelActividad = 1.55;
+        break;
+      case 3:
+        nivelActividad = 1.725;
+        break;
+      case 4:
+        nivelActividad = 1.9;
+        break;
+      default:
+        nivelActividad = 1.2;
+    }
     setState(() {
       _selectedButtonIndex = index;
+      selectednivelActividad = nivelActividad;
+    });
+  }
+
+  void _submitnivelActividad(double nivelActividad) async {
+    final db = FirebaseFirestore.instance;
+    final documentRef = db.collection("Users").doc(id);
+
+    await documentRef.update({
+      'nivelActividad': nivelActividad,
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Column(
-      children: [
-        Container(
-          height: MediaQuery.of(context).size.height * 0.1,
-          color: Colors.blue,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        // ignore: avoid_unnecessary_containers
-        Container(
-          child: const Center(
-            child: Text(
-              'Nivel de Actividad',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Column(
+      body: SafeArea(
+        child: Column(
           children: [
-            _buildButtonColumn(
-                0,
-                "Poco o ningun ejercicio",
-                const Icon(
-                  Icons.directions_walk,
-                  size: 50,
-                  color: kWhiteColor,
-                )),
-            _buildButtonColumn(
-                1,
-                "Ejercicio ligero (1-3 días)",
-                const Icon(
-                  Icons.directions_bike,
-                  size: 50,
-                  color: kWhiteColor,
-                )),
-            _buildButtonColumn(
-                2,
-                "Ejercicio Moderado(3-5 días)",
-                const Icon(
-                  Icons.directions_run,
-                  size: 50,
-                  color: kWhiteColor,
-                )),
-            _buildButtonColumn(
-                3,
-                "Ejercicio fuerte (6-7 días)",
-                const Icon(
-                  Icons.fitness_center,
-                  size: 50,
-                  color: kWhiteColor,
-                )),
-            _buildButtonColumn(
-                4,
-                "Ejercicio Intenso (2 vcs x día)",
-                const Icon(
-                  Icons.accessibility_new,
-                  size: 50,
-                  color: kWhiteColor,
-                )),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 18.0),
-              width: MediaQuery.of(context).size.width * .4,
-              decoration: BoxDecoration(
-                  color: Colors.lightBlue[500],
-                  borderRadius: BorderRadius.circular(20.0)),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 2.0, bottom: 4.0),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.Presetting2Page);
-                  },
-                  child: const Text(
-                    "Acceder",
-                    style:
-                        TextStyle(color: kDefaultIconLightColor, fontSize: 22),
-                  ),
-                ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Center(
+              child: Text(
+                'Nivel de Actividad',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                _buildButtonColumn(
+                  0,
+                  "Poco o ningún ejercicio",
+                  const Icon(
+                    Icons.directions_walk,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                _buildButtonColumn(
+                  1,
+                  "Ejercicio ligero (1-3 días)",
+                  const Icon(
+                    Icons.directions_bike,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                _buildButtonColumn(
+                  2,
+                  "Ejercicio Moderado(3-5 días)",
+                  const Icon(
+                    Icons.directions_run,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                _buildButtonColumn(
+                  3,
+                  "Ejercicio fuerte (6-7 días)",
+                  const Icon(
+                    Icons.fitness_center,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                _buildButtonColumn(
+                  4,
+                  "Ejercicio Intenso (2 vcs x día)",
+                  const Icon(
+                    Icons.accessibility_new,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 18.0),
+                  width: MediaQuery.of(context).size.width * .4,
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue[500],
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 2.0, bottom: 4.0),
+                    child: TextButton(
+                      onPressed: () {
+                        print("Valor seleccionado: $selectednivelActividad");
+                        _submitnivelActividad(selectednivelActividad);
+                        Navigator.pushNamed(context, Routes.Presetting2Page, arguments: id);
+                      },
+                      child: const Text(
+                        "Acceder",
+                        style: TextStyle(
+                          color: kDefaultIconLightColor,
+                          fontSize: 22,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
-        )
-      ],
-    )));
+        ),
+      ),
+    );
   }
 
   Widget _buildButtonColumn(int index, String text, Icon icon) {
     final isSelected = _selectedButtonIndex == index;
-
-    // ignore: sized_box_for_whitespace
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10.0),
       width: MediaQuery.of(context).size.width * 0.78,
@@ -127,7 +170,8 @@ class _PreSettingsViewState extends State<PreSettingsView> {
           style: TextButton.styleFrom(
             backgroundColor: isSelected ? Colors.blue : Colors.grey[400],
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
           ),
           child: Row(
             children: [
@@ -138,7 +182,9 @@ class _PreSettingsViewState extends State<PreSettingsView> {
               Text(
                 text,
                 style: const TextStyle(
-                    color: kDefaultIconLightColor, fontSize: 15),
+                  color: kDefaultIconLightColor,
+                  fontSize: 15,
+                ),
               ),
             ],
           ),
